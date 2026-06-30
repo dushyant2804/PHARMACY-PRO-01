947778a6f2f8cd02817659c0bc08fab395e8b58d
dushyant2804 <dushyantbhadu07@gmail.com>
2026-06-30 12:50:10 +0530

    Add backend update notification endpoints

---
+++ app_version.py
@@ -0,0 +1,9 @@
+"""Centralized PharmacyOS backend application version.
+
+This module is the single source of truth for the backend runtime version used
+by the application-level version and update notification APIs.
+"""
+
+APP_VERSION = "1.0.0"
+APP_BUILD = 1
+APP_CHANNEL = "stable"
